ea37e885f2e153853c193e511122d52220033430
cwayade <celiane.ayad@gmail.com>
2022-08-19 15:12:25 +0200

    added xor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name='shapleychains',
-    version='0.1.0',
+    version='0.1.1',
     description='',
     url= 'https://github.com/cwayad/shapleychains.git',
     long_description=long_description,
